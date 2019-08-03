7b9b0d026c3828502e24b709c3a2a04755273634
0xmmalik <manavmalik36@gmail.com>
2019-08-03 11:20:07 -0400

    reformatted program and redid mutable default arguments

--- pycard.py
+++ pycard.py
@@ -7,11 +7,23 @@ TABLE OF CONTENTS IS ON BOTTOM LINE
 #####################
 from random import shuffle
 
+
 ##############
 # DECK CLASS #
 ##############
 class Deck:
-    def __init__(self, modifiers = ["Hearts", "Diamonds", "Spades", "Clubs"], values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], text = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"], prFormat = "DECKTXT of DECKMOD"):
+    def __init__(self, modifiers=None,
+                 values=None,
+                 text=None,
+                 prFormat="DECKTXT of DECKMOD",
+                 ):
+        if values is None:
+            values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+        if modifiers is None:
+            modifiers = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        if text is None:
+            text = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen",
+                    "King"]
         self.cards = []
         self.format = prFormat
         self.hands = []
@@ -20,14 +32,15 @@ class Deck:
             for modifier in modifiers:
                 self.cards.append((values[i], text[i], modifier))
 
-    def printCardsStr(self, delimiter = ", ", endlist = '\n'):
+    def printCardsStr(self, delimiter=", ", endlist='\n'):
         for i in range(len(self.cards)):
             card = self.cards[i]
-            print(self.format.replace("DECKTXT", card[1]).replace("DECKMOD", card[2]).replace("DECKVAL", str(card[0])), end='')
+            print(self.format.replace("DECKTXT", card[1]).replace("DECKMOD", card[2]).replace("DECKVAL", str(card[0])),
+                  end='')
             if i != len(self.cards) - 1:
                 print(delimiter, end='')
             else:
-                print("", end = endlist)
+                print("", end=endlist)
 
     def printSingleStr(self, index):
         card = self.cards[index]
@@ -35,21 +48,22 @@ class Deck:
 
     def getSingleStr(self, index):
         card = self.cards[index]
-        return (self.format.replace("DECKTXT", card[1]).replace("DECKMOD", card[2]).replace("DECKVAL", str(card[0])))
+        return self.format.replace("DECKTXT", card[1]).replace("DECKMOD", card[2]).replace("DECKVAL", str(card[0]))
 
     def getSingleUnused(self, index):
-        #TODO: implement
+        # TODO: implement
         pass
 
     def shuffleUnused(self, index):
-        #TODO: implement
+        # TODO: implement
         pass
+
 
 '''
 TABLE OF CONTENTS:
 
 Import Statements ........................ Line 5
-Deck Class ............................... Line 10
-Button Clas .............................. Line #
+Deck Class ............................... Line 11
+Button Class .............................. Line #
 
 '''
